0f7b87e10a2007a9c6d3975f56d2d69c1ad70dcb
Maria Dobrydnyk <mariadobrydnyk@MacBook-Air-Maria.local>
2026-01-03 13:25:46 +0200

    update styles making responsive design

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 const swiper = new Swiper('.projects__slider', {
     loop: true,
-    slidesPerView: 3,
     spaceBetween: 20,
 
     navigation: {
       nextEl: '.projects__arrow-right',
       prevEl: '.projects__arrow-left',
+    },
+
+    breakpoints: {
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
     },
   });
